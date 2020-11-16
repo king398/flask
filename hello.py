6a068c524e2480f639b6e7e5932623e28a2dde0b
king398 <mithilsalunkhe@outlook.com>
2020-11-16 09:17:54 +0530

    d

--- hello.py
+++ hello.py
@@ -1,15 +1,1 @@
 from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_wrld():
-	return "Hello ddld", "Mithil"
-
-
-if __name__ == '__main__':
-	app.debug = True
-	app.run()
-	app.run(debug=True)
-
