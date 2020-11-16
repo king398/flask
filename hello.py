3b4a569eb8c8b106a4e9d483eb0de67bfa2a7a85
king398 <mithilsalunkhe@outlook.com>
2020-11-16 10:30:13 +0530

    Update the project

--- hello.py
+++ hello.py
@@ -10,6 +10,9 @@ class Todo(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	content = db.Column(db.String(200), nullable=False)
 
+	def __repr__(self):
+		return '<Task r%' % self.id
+
 
 @app.route("/")
 def index():
