9451d596fcac894a092682f449a14bf81036cb12
king398 <mithilsalunkhe@outlook.com>
2020-11-16 10:23:26 +0530

    Update the project

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQL_ALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	content = db.Column(db.String(200), nullable=False)
 
 
 @app.route("/")
